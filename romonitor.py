911b6806a87a5092f62eeb46e4250b2320ccdb58
Autumn <autumn@autmn.net>
2024-08-03 14:42:31 -0600

    datetime handling, better object comparison; needs work

--- romonitor.py
+++ romonitor.py
@@ -8,13 +8,14 @@ https://github.com/aut-mn
 """
 import asyncio
 import argparse
-import signal
 import datetime
 import json
 import sys
 import logging
-from typing import Any, List, Tuple
-from dataclasses import dataclass
+import time
+import re
+from typing import Any, Tuple, List
+from dataclasses import dataclass, fields, is_dataclass
 
 import aiohttp
 
@@ -39,7 +40,7 @@ class Item:
     economics: Economy
     description: str | None
     creator: str
-    updated: datetime.datetime
+    updated: datetime.datetime | None
     thumbnail: str = "https://rowhois.com/not-available.png"
 
 
@@ -58,35 +59,47 @@ class Authentication:
     xcsrf: Token
 
 
-def compare(item1: Item, item2: Item) -> List[str] | None:
-    """Compares this item with another item and returns a list of attributes that have changed"""
-    changed_attributes = []
-    for attribute in vars(item1):
-        if getattr(item1, attribute) != getattr(item2, attribute):
-            changed_attributes.append(attribute)
-            old_value = getattr(item1, attribute)
-            new_value = getattr(item2, attribute)
-            logger.debug(
-                "Diff: %s: %s -> %s",
-                attribute, old_value if old_value not in [None, ''] else 'None',
-                new_value if new_value not in [None, ''] else 'None'
+def fancy_time(initstamp: Any) -> datetime.datetime | None:
+    """Converts a datetime string to a datetime object."""
+    if isinstance(initstamp, (int, float)):
+        return datetime.datetime.fromtimestamp(initstamp, tz=datetime.timezone.utc)
+    if isinstance(initstamp, str):
+        match = re.match(r"(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(\.\d+)?Z", initstamp)
+        if match:
+            year, month, day, hour, minute, second, _ = match.groups()
+            return datetime.datetime(
+                int(year), int(month), int(day), int(hour), int(minute), int(second),
+                tzinfo=datetime.timezone.utc
             )
-    if changed_attributes:
-        logger.debug(
-            "Item data changed, returning key%s %s",
-            's' if len(changed_attributes) >= 2 else '', changed_attributes
-        )
-    return changed_attributes if changed_attributes else None
+    return None
+
+
+def compare(obj1, obj2, parent_key='') -> List[str]:
+    """Compares two dataclasses and returns a list of changed keys"""
+    changes = []
+
+    def key_path(accesskey):
+        return f"{parent_key}.{accesskey}" if parent_key else accesskey
+    for field in fields(obj1):
+        accesskey = field.name
+        kpath = key_path(accesskey)
+        value1 = getattr(obj1, accesskey)
+        value2 = getattr(obj2, accesskey)
+        if is_dataclass(value1) and is_dataclass(value2):
+            changes.extend(compare(value1, value2, kpath))
+        elif value1 != value2:
+            logger.debug("Diff '%s': '%s' -> '%s'", kpath, value1, value2)
+            changes.append(kpath)
+    return changes
 
 
 async def token_renewal() -> Token:
     """Updates the x-csrf-token"""
     try:
-        if debug_mode:
-            logger.debug("Renewing X-CSRF Token")
+        logger.debug("%s X-CSRF Token", "Renewing" if x_token.token else "Fetching")
         async with aiohttp.ClientSession(cookies={".roblosecurity": roblosecurity}) as session:
             async with session.post("https://auth.roblox.com/v2/logout") as resp:
-                if 'x-csrf-token' not in resp.headers and debug_mode:
+                if 'x-csrf-token' not in resp.headers:
                     logger.debug("Failed to renew token: %d", resp.status)
         return Token(datetime.datetime.now(), resp.headers.get('x-csrf-token', None))
     except Exception:  # noqa: W0718
@@ -95,10 +108,9 @@ async def token_renewal() -> Token:
 
 async def validate_cookie() -> bool:
     """Validates the roblosecurity value from config.json. Returns True if valid."""
-    global x_token
+    global x_token  # noqa: W0603
     try:
-        if debug_mode:
-            logger.debug("Validating roblosecurity cookie")
+        logger.debug("Validating roblosecurity cookie")
         async with aiohttp.ClientSession(cookies={".roblosecurity": roblosecurity}) as main_session:
             async with main_session.get("https://users.roblox.com/v1/users/authenticated") as resp:
                 if resp.status == 200:
@@ -115,10 +127,10 @@ async def validate_cookie() -> bool:
 
 async def rofetch(url: str, method: str = "get", debugmessage: str | None = None, **kwargs) -> tuple[int, Any] | None:  # noqa: E501
     """Fetches authenticated Roblox URLs. Error handler included. Free Robux not included."""
-    global x_token
+    global x_token  # noqa: W0603
     try:
         async with aiohttp.ClientSession(cookies={".roblosecurity": roblosecurity}) as session:
-            if debug_mode and debugmessage:
+            if debugmessage:
                 logger.debug(debugmessage)
             for _ in range(5):
                 response = await session.request(
@@ -163,12 +175,13 @@ async def get_item(asset: int) -> Item | None:
         if data is None:
             return None
         economy_data = Economy(
-            data[1]['IsForSale'],
-            data[1]['PriceInRobux'],
-            data[1]['IsLimited'],
-            data[1]['IsLimitedUnique'],
-            data[1]['CollectiblesItemDetails']['IsLimited'] if data[1].get('CollectiblesItemDetails') is not None else False,  # noqa: E501
-            data[1]['Remaining'] if data[1].get('Remaining') is not None else None
+            forsale=data[1]['IsForSale'],
+            price=data[1]['PriceInRobux'],
+            resaleprice=None,
+            limited=data[1]['IsLimited'],
+            limitedu=data[1]['IsLimitedUnique'],
+            collectible=data[1]['CollectiblesItemDetails']['IsLimited'] if data[1].get('CollectiblesItemDetails') is not None else False,  # noqa: E501
+            remaining=data[1]['Remaining'] if data[1].get('Remaining') is not None else None
         )
         if economy_data.collectible or economy_data.limited or economy_data.limitedu:
             resale = await fetch_resale(asset)
@@ -176,7 +189,7 @@ async def get_item(asset: int) -> Item | None:
         return_item = Item(
             id=asset, name=data[1]['Name'], economics=economy_data,
             description=data[1]['Description'], creator=data[1]['Creator']['Name'],
-            updated=datetime.datetime.strptime(data[1]['Updated'], "%Y-%m-%dT%H:%M:%S.%fZ"),
+            updated=fancy_time(data[1]['Updated']),
         )
         if data[0] != 200:
             logger.error("Failed to fetch item data: %d", data[0])
@@ -188,8 +201,7 @@ async def get_item(asset: int) -> Item | None:
 
 async def send_webhook(message: str, title: str | None = None, url: str | None = None) -> None:
     """Sends a message through the webhook"""
-    if debug_mode:
-        logger.debug("Pushing to webhook")
+    logger.debug("Pushing to webhook")
     webhook_contents = {
         "username": "RoMonitor", "avatar_url": "https://rowhois.com/builderman.png",
         "content": f"<@{mention}>" if mention != 0 else None,
@@ -210,7 +222,7 @@ async def validate_item() -> Tuple[bool, str | None]:
     data, item_thumbnail = await asyncio.gather(
         rofetch(
             f"https://economy.roblox.com/v2/assets/{item}/details",
-            debugmessage="Fetching latest item details"
+            debugmessage="Validating asset"
         ),
         rofetch(
             f"https://thumbnails.roblox.com/v1/assets?assetIds={item}&size=420x420&format=Png",
@@ -226,17 +238,10 @@ async def validate_item() -> Tuple[bool, str | None]:
             logger.fatal("The specified item does not exist.")
         else:
             logger.fatal("Failed to initialize. Item '%s' seems to be invalid.", item)
-        if debug_mode:
-            logger.debug("Got code %d during initialization", data[0])
+        logger.debug("Got code %d during initialization", data[0])
         return False, "https://rowhois.com/not-available.png"
     item_thumbnail = item_thumbnail[1]['data'][0]['imageUrl']
     return True, item_thumbnail
-
-
-async def shutdown() -> None:  # Will error if ran during init
-    """Closes the client and cancels all tasks in the loop"""
-    for task in asyncio.all_tasks(loop):
-        task.cancel()
 
 
 parser = argparse.ArgumentParser(
@@ -267,6 +272,7 @@ formatter = logging.Formatter(
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
+
 logger.info("Initializing RoMonitor...")
 item = args.item
 mention = args.mention
@@ -274,8 +280,9 @@ with open('config.json', 'r', encoding='utf-8') as configfile:
     config = json.load(configfile)
     roblosecurity = config['roblosecurity']
     webhookURL = config['webhook']
-    debug_mode = config['debug']
-    for key in [config, roblosecurity, webhookURL, debug_mode]:
+    if not config['debug']:
+        logger.setLevel(logging.INFO)
+    for key in [config, roblosecurity, webhookURL]:
         if key == "":
             raise KeyError
 
@@ -287,27 +294,59 @@ itemValid, thumbnail = loop.run_until_complete(validate_item())
 if not itemValid:
     logger.fatal("Invalid item provided!")
     sys.exit(1)
+
+main_item = loop.run_until_complete(get_item(item))
+if main_item is None:
+    logger.error("Item [\033[94m%s\033[0m] is not a valid item.", item)
+    sys.exit(1)
+
+loop.run_until_complete(send_webhook(
+    f"RoMonitor is now monitoring `{main_item.name}` by `{main_item.creator}`",
+    title=f"{main_item.name}", url=f"https://www.roblox.com/catalog/{item}/"
+))
+logger.info(
+    "Initialized! Now monitoring [\033[94m%s\033[0m] by [\033[94m%s\033[0m]",
+    main_item.name, main_item.creator
+)
+
+# Here's the bread and butter of the program if you will
 try:
-    main_item = loop.run_until_complete(get_item(item))
-    if main_item is None:
-        logger.error("Item [\033[94m%s\033[0m] is not a valid item.", item)
-        sys.exit(1)
-    loop.run_until_complete(send_webhook(
-        f"RoMonitor is now monitoring `{main_item.name}` by `{main_item.creator}`.",
-        title=f"{main_item.name}", url=f"https://www.roblox.com/catalog/{item}/"
-    ))
-    logger.info(
-        "Initialized! Now monitoring [\033[94m%s\033[0m] by [\033[94m%s\033[0m]",
-        main_item.name, main_item.creator
-    )
-    # if datetime.datetime.now() - x_token.datetime >= datetime.timedelta(minutes=5):
-    #     x_token = await token_renewal()
-    # if not x_token.token:
-    #     logger.error("Token renewal failed. Account Session Protection enabled?")
-    loop.add_signal_handler(signal.SIGINT, lambda: asyncio.create_task(shutdown()))
+    while True:
+        time.sleep(args.time + 5 if args.time <= 5 else args.time)
+        if datetime.datetime.now() - x_token.datetime >= datetime.timedelta(minutes=5):
+            x_token = loop.run_until_complete(token_renewal())
+            if not x_token.token:
+                logger.error("Token renewal failed. Account Session Protection enabled?")
+                continue
+        if main_item is None:
+            raise ValueError("main_item is None")
+        old_item = main_item
+        # ^ Could result in unbound error if main_item is None but shouldn't happen
+        main_item = loop.run_until_complete(get_item(item))
+        if main_item is not None:
+            CHANGES = compare(old_item, main_item)
+            if CHANGES:
+                if  'economics.price' in CHANGES:
+                    loop.run_until_complete(
+                        send_webhook(
+                            title=f"{old_item.name}",
+                            url=f"https://www.roblox.com/catalog/{main_item.id}",
+                            message=f"""`{main_item.name}` is now {main_item.economics.price} {'Robux' if main_item.economics.price != None else 'Free'}!
+                            **Old price**: `{old_item.economics.price}`
+                            **New price**: `{main_item.economics.price}`
+                            """  # noqa: E501
+                        )
+                    )
+                if args.runforever is False:
+                    break
+            else:
+                main_item = old_item
 except KeyboardInterrupt:
     pass
 except asyncio.CancelledError:
     pass
+
 logger.info("Exiting RoMonitor. Have a nice day!")
+for task in asyncio.all_tasks(loop):
+    task.cancel()
 sys.exit(0)
